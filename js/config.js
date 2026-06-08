93a3948c9ca2ddc16e897b2712a35cf36b128cdd
Sinthujan Kulasekaram <ksinthujan2010@gmail.com>
2026-06-08 14:30:40 +1000

    init

---
+++ js/config.js
@@ -0,0 +1,11 @@
+// These are commented out in local development because of local .env.* files
+// that pass configuration to the app.
+// During deployment, this file should be replaced with one that contains
+// the variables uncommented and with the actual configuration
+
+// Target environment. For DevCloud we have special handling
+// Possible values: "devcloud" | "production"
+// window.REACT_APP_TARGET_ENV = 'devcloud';
+
+// App API URL
+// window.REACT_APP_API_URL = 'https://example.com/api';
